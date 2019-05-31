3609d49d919f41cb798cd38622a6a278924397af
quadrant26 <quadrant26@163.com>
2019-05-31 18:28:15 +0800

    something

--- demo01/src/App.js
+++ demo01/src/App.js
@@ -8,10 +8,16 @@ import React,{Component} from 'react'
 class App extends Component{
     render(){
         return (
-            <div>
-                Hello World!
-            </div>
+            <ul className="My-list">
+                <li>react.com</li>
+                <li>I love React</li>
+                <li>{true?"King":"Queen"}</li>
+            </ul>
         )
+
+        var child1 = React.createElement("li", null, "React.com");
+        var child2 = React.createElement("li", null, "I love React");
+        var root = React.createElement("ul", null, {className: "my-list"}, child1, child2 );
     }
 }
 
