6f00f121e7c25e98555b179a5bd4029b4279c807
quadrant26 <quadrant26@163.com>
2019-06-03 16:53:20 +0800

    初始化数据，数据绑定和添加事件

--- demo01/src/xiaojiejie.js
+++ demo01/src/xiaojiejie.js
@@ -2,19 +2,59 @@ import React, {Component, Fragment} from 'react'
 import ReactDom from 'react-dom'
 
 class Xiaojiejie extends Component{
+    // 构造函数初始化
+    constructor(props){
+        super(props)
+        this.state = {
+            inputValue: '',
+            list: ['基础按摩', '精油推背']
+        }
+    }
 
     render(){
         return (
             <Fragment>
                 <div>
-                    <input type="text" /><button>增加服务</button>
+                    <input type="text" value={this.state.inputValue} onChange={this.inputChange.bind(this)} />
+                    <button onClick={this.addList.bind(this)}>增加服务</button>
                 </div>
                 <ul>
-                    <li>头部按摩</li>
-                    <li>精油推背</li>
+                    {
+                        this.state.list.map((item, index)=>{
+                            return <li key={index}>{item}</li>
+                        })
+                    }
                 </ul>
             </Fragment>
         )
+    }
+
+    /**
+     * Each child in a list should have a unique "key" prop.
+     ** return <li key={index}>{item}</li>
+     */
+
+    inputChange (e){
+        console.log(e.target.value)
+        /**
+         * Error
+         * 1. this 指向错误 需要给事件绑定 this
+         ** onChange={this.inputChange.bind(this)}
+         * 2. React中改变值需要使用this.setState方法
+         ** this.setState({ inputValue: e.target.value })
+         */
+        // this.state.inputValue = e.target.value
+
+        this.setState({
+            inputValue: e.target.value
+        })
+    }
+
+    // 增加列表
+    addList (){
+        this.setState({
+            list: [...this.state.list, this.state.inputValue]
+        })
     }
 }
 
