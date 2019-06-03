a6814fb87b8130331617eafb3b5d74d262847e67
quadrant26 <quadrant26@163.com>
2019-06-03 17:33:05 +0800

    create component

---
+++ demo01/src/XiaojiejieItem.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class XiaojijieItem extends Component {
+    
+    render() { 
+        return (
+            <li>小姐姐</li>
+        );
+    }
+}
+ 
+export default XiaojijieItem;
