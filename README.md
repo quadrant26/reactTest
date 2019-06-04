e072005a7c60eeadb62cce031157f50d46bbd421
quadrant26 <quadrant26@163.com>
2019-06-04 15:39:34 +0800

    PropTypes 校验

--- README.md
+++ README.md
@@ -32,3 +32,30 @@ react study
             console.log(this.props.index);
             this.props.deleteItem(this.props.index)
         }
+
+2. 单项数据流
+
+    子组件无法直接修改传递的值，只能父组件传递方法，然后调用父组件传递的方法进行改变
+
+3. PropTypes 
+
+    校验传递值
+
+        import PropTypes from 'prop-types';
+
+        XiaojijieItem.propTypes = {
+            avname: PropTypes.string.isRequired, // 必须传递
+            content: PropTypes.string,
+            index: PropTypes.number,
+            deleteItem: PropTypes.func
+        }
+
+    设置默认值
+
+        XiaojijieItem.defaultProps = {
+            avname: "xxx"
+        }
+
+4. ref
+
+    
--- demo01/src/XiaojiejieItem.js
+++ demo01/src/XiaojiejieItem.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class XiaojijieItem extends Component {
 
@@ -9,7 +10,9 @@ class XiaojijieItem extends Component {
     
     render() { 
         return (
-            <li onClick={this.handleClick}>{this.props.content}</li>
+            <li onClick={this.handleClick}>
+            {this.props.avname} 为你服务-{this.props.content}
+            </li>
         );
     }
 
@@ -17,6 +20,17 @@ class XiaojijieItem extends Component {
         console.log(this.props.index);
         this.props.deleteItem(this.props.index)
     }
+}
+
+XiaojijieItem.propTypes = {
+    avname: PropTypes.string.isRequired,
+    content: PropTypes.string,
+    index: PropTypes.number,
+    deleteItem: PropTypes.func
+}
+
+XiaojijieItem.defaultProps = {
+    avname: "初音未来"
 }
  
 export default XiaojijieItem;
